50abd0d2c122850ce5ad8070b7c5a52761cbe019
wibeasley <wibeasley@hotmail.com>
2017-04-15 12:52:54 -0500

    Update during Botball challenge day

---
+++ 1397-wallaby/Default User/rescue the can/src/main.c
@@ -0,0 +1,45 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    
+    enable_servos();
+    set_servo_position(2,1457);
+    set_servo_position(1,1803);
+    
+    
+    
+    motor(0,50);
+    motor(3,50);
+    msleep(6000);
+    
+    enable_servos();
+    set_servo_position(2,1457);
+    set_servo_position(1,1803);
+    
+    
+    
+    ao();
+    msleep(200);
+    
+    
+    set_servo_position(2,703);
+    
+    motor(0,-50);
+    motor(3,-50);
+    msleep(6000);
+    
+    set_servo_position(1,1359);
+    
+    
+    motor(3,30);
+    msleep(800);
+    
+    motor(0,30);
+    motor(3,30);
+    msleep(700);
+    
+    
+    ao();
+    return 0;
+}
