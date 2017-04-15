a51fbe540ebb20271db39df8865b89176015d784
wibeasley <wibeasley@hotmail.com>
2017-04-15 12:22:13 -0500

    Update during Botball challenge day

--- 1395-wallaby/Default User/dance party/src/main.c
+++ 1395-wallaby/Default User/dance party/src/main.c
@@ -3,5 +3,49 @@
 int main()
 {
     printf("Hello World\n");
+   
+   motor(0,50);
+    motor(3,50);
+    msleep(3000);
+    
+    
+    motor(0,50);
+    motor(3,00);
+    msleep(6000);
+    
+    motor(0,50);
+    motor(1,50);
+    msleep(3000);
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
--- 1408-wallaby/Default User/challenge-20/src/main.c
+++ 1408-wallaby/Default User/challenge-20/src/main.c
@@ -5,7 +5,7 @@ int main()
 enable_servos(1);
 enable_servos(0);
       set_servo_position(0,1618);
-  set_servo_position(1,1630);
+  set_servo_position(1,1646);
     
     motor(3,50);
     motor(0,50);
@@ -17,21 +17,27 @@ enable_servos(0);
     
     motor(0,-50);
     motor(3,-50);
-    msleep(6500);
+    msleep(7000);
     ao();
+   
+       set_servo_position(1,961);
+    msleep(1000);
     
     motor(3,50);
     msleep(1500);
     ao();
     
-     set_servo_position(1,961);
+  
+    
+    set_servo_position(1,1572);
     msleep(1000);
     
     set_servo_position(0,1618);
     msleep(1000);
     
-   
-    
+    motor(0,-50);
+    msleep(2000);
+    ao();
     printf("Hello World\n");
     return 0;
 }
