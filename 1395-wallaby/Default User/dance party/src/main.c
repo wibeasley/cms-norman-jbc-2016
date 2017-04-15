52905b412a0c0f386a9bd71cd8ed6ab2eaa7e928
wibeasley <wibeasley@hotmail.com>
2017-04-15 13:33:48 -0500

    Update during Botball challenge day

--- 1395-wallaby/Default User/dance party/src/main.c
+++ 1395-wallaby/Default User/dance party/src/main.c
@@ -2,29 +2,35 @@
 
 int main()
 {
-    printf("Hello World\n");
-   
+    printf("Dance Party\n");
+    
+   printf("gooing forward1\n");
    motor(0,50);
     motor(3,50);
     msleep(3000);
     
-    
+    printf("turning1\n");
     motor(0,50);
     motor(3,00);
+    msleep(8000);
+    
+    printf("going forward 2\n");
+    motor(0,50);
+    motor(3,50);
     msleep(3000);
     
-    motor(0,50);
-    motor(1,50);
-    msleep(3000);
-       
+    printf("turning 2\n");   
     motor(1,50);
     motor(0,00);
-    msleep(3000);
+    msleep(8000);
     
+    enable_servos();
+    set_servo_position(0, 991);
+    set_servo_position(0, 148);
     
-    
-    
-    
+    motor(1,50);
+    motor(0,50);
+    msleep(-3000);
     
     
     
