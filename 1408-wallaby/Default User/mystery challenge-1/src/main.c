17c22c0a44bd75cb4247eab2760591312c2f35b6
wibeasley <wibeasley@hotmail.com>
2017-04-15 11:07:06 -0500

    Update during Botball challenge day

--- 1408-wallaby/Default User/mystery challenge-1/src/main.c
+++ 1408-wallaby/Default User/mystery challenge-1/src/main.c
@@ -2,6 +2,43 @@
 
 int main()
 {
+         enable_servos(0);
+    enable_servos(1);
+    set_servo_position(0,1572);
+    
+    motor(3,50);
+    motor(0,50);
+    msleep(800);
+    ao();
+    
+     set_servo_position(0,1554);
+    msleep(1000);
+    
+    set_servo_position(1,1608);
+   msleep(1000);
+    
+    set_servo_position(0,889);
+    msleep(1000);
+    
+    set_servo_position(1,819);
+    msleep(1000);
+    
+   motor(0,50);
+    msleep(1500);
+    ao();
+    
+        
+    set_servo_position(0,1466);
+    msleep(1000);
+    
+    motor(3,50);
+    msleep(500);
+    ao();
+    
+   motor(3,50);
+    msleep(1000);
+    ao();  
+    
     printf("Hello World\n");
     return 0;
 }
