434a42bb80bd83069f8cdf93ca9d15af18d1e3a0
wibeasley <wibeasley@hotmail.com>
2017-12-02 12:57:00 -0600

    Update during botball event -fall 2017

---
+++ 1395-wallaby/Default User/linedance/src/main.c
@@ -0,0 +1,61 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    printf("turn left\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    printf("turn right\n");
+    motor(3,50);
+    motor(0,20);
+    msleep(1000);
+    
+    printf("turn left\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+   printf("turn right\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000); 
+    
+    
+    printf("turn left\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    printf("turn right\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    
+    printf("turn left\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    
+    printf("turn right\n");
+    motor(3,50);
+    motor(0,20);
+    msleep(1000);
+    
+    printf("turn left\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    printf("turn right\n");
+    motor(0,50);
+    motor(3,20);
+    msleep(1000);
+    
+    
+    
+    return 0;
+}
--- connect/harvest-1.sh
+++ connect/harvest-1.sh
@@ -7,11 +7,11 @@
 #   If a wallaby is missing or out of range, the script will scipt over it after the nmcli times out.
 #   Or temporarily comment out the Wallaby in the array below.
 bots=(
-  #"1395-wallaby"  # H & B
+  "1395-wallaby"  # H & B
   #"1397-wallaby"  # J & L
   #"1399-wallaby"  # E & V          # USB
   #"1407-wallaby"  # M & B          # USB
-  "1408-wallaby"  # M & M          # USB
+  #"1408-wallaby"  # M & M          # USB
   #"2486-wallaby"  # V & C
   #"2488-wallaby"  # M & A
   #"2494-wallaby"  # D & L
