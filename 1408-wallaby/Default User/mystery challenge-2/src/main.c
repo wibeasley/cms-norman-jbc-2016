0d7877fe7ad8e1fb3cfdc955bf9716bf4704aa23
wibeasley <wibeasley@hotmail.com>
2017-12-02 11:50:03 -0600

    Update during botball event -fall 2017

--- 1408-wallaby/Default User/mystery challenge-2/src/main.c
+++ 1408-wallaby/Default User/mystery challenge-2/src/main.c
@@ -19,7 +19,102 @@ int main()
     msleep(2000);
     ao();
     
+    printf("go forward\n");
+    motor(0,50);
+    motor(3,50);
+    msleep(1500);
+    ao();
+    
+    printf("turn right\n");
+    motor(3,50);
+    msleep(2000);
+    ao();
+    
+    printf("go forward\n");
+    motor(0,50);
+    motor(3,50);
+    msleep(2000);
+    ao();
+    
+    printf("turn right\n");
+     motor(3,50);
+    msleep(1800);
+    ao();
+    
+    printf("go forward\n");
+    motor(0,50);
+    motor(3,50);
+    msleep(2000);
+    ao();
+    
+     printf("turn right\n");
+     motor(3,50);
+    msleep(1800);
+    ao();
+    
+    printf("go forward\n");
+    motor(0,50);
+    motor(3,50);
+    msleep(2000);
+    ao();
+    
+    printf("stop\n");
+    motor(0,0);
+    motor(3,0);
+    msleep(1000);
+    ao();
+    
+    printf("go backward\n");
+    motor(0,-50);
+    motor(3,-50);
+    msleep(2500);
+    ao();
+    
+    printf("turn left\n");
+    motor(3,-50);
+    msleep(1800);
+    ao();
+    
+    printf("go backward\n");
+    motor(0,-50);
+    motor(3,-50);
+    msleep(2300);
+    ao();
+    
+    printf("turn right\n");
+    motor(0,-50);
+    msleep(1800);
+    ao();
+    
+    printf("go backward\n");
+    motor(0,-50);
+    motor(3,-50);
+    msleep(2300);
+    ao();
+    
+    printf("turn right\n");
+    motor(0,-50);
+    msleep(2000);
+    ao();
+    
+    printf("go backward\n");
+    motor(0,-50);
+    motor(3,-50);
+    msleep(2500);
+    ao();
+    
+    printf("turn right\n");
+    motor(0,-50);
+    msleep(2000);
+    ao();
+    
+    printf("go backward\n");
+    motor(0,-50);
+    motor(3,-50);
+    msleep(8000);
+    ao();
+    
     printf("Hello World\n");
-    printf("Hello World\n");
+    printf("right 7 by 3\n");
     return 0;
 }
