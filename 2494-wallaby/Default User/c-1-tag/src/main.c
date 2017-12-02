78ce4cf068a735be5892f575bde766b82921f255
wibeasley <wibeasley@hotmail.com>
2017-12-02 11:09:05 -0600

    Update during botball event -fall 2017

--- 2494-wallaby/Default User/c-1-tag/src/main.c
+++ 2494-wallaby/Default User/c-1-tag/src/main.c
@@ -2,6 +2,20 @@
 
 int main()
 {
-    printf("Hello World\n");
-    return 0;
+
+    
+printf("go forward/n");
+motor(0,28);
+motor(3,28);
+msleep(10000);   
+    
+printf("turn right/n");
+motor(3,60);
+msleep(8000);  
+
+printf("go forward/n");
+motor(0,28);
+motor(3,28);
+msleep(10000); 
+return 0;
 }
