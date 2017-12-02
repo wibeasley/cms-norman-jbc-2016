1faed0684d861333c05532e1cbf4f6d5d8277c60
wibeasley <wibeasley@hotmail.com>
2017-12-02 11:37:11 -0600

    Update during botball event -fall 2017

--- 2486-wallaby/Default User/ring around the can/src/main.c
+++ 2486-wallaby/Default User/ring around the can/src/main.c
@@ -3,7 +3,24 @@
 int main()
 {
     printf("go forward\n");
-    motor(3,10);
- msleep(5000);
+    motor(3,15);
+    motor(0,15);
+    msleep(20000);
+    printf("left turn\n");
+    motor(0,52);
+    msleep(2500);
+     printf("go forward\n");
+    motor(0,52);
+    motor(3,52);
+    msleep(2500);
+     printf("left turn\n");
+    motor(0,52);
+    motor(3,0);
+    msleep(3000);
+      printf("go forward\n");
+    motor(3,70);
+    motor(0,70);
+    msleep(6000);
     return 0;
+    
 }
--- 2488-wallaby/Default User/ring around the can/src/main.c
+++ 2488-wallaby/Default User/ring around the can/src/main.c
@@ -2,6 +2,19 @@
 
 int main()
 {
-    printf("Hello World\n");
+    printf("go forward/n");
+    motor(0,30);
+motor(3,30);
+    msleep(10000);
+printf("turn/n");
+    motor(0,10);
+ motor(3,30);   
+msleep(12000);    
+printf("go forward/n");
+    motor(0,30);
+    motor(0,30);
+    msleep(10000);
+          
+    
     return 0;
 }
