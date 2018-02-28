bfb4b2c9336eb0cf528543aba6d67a885d4fb2e3
wibeasley <wibeasley@hotmail.com>
2018-02-28 14:59:47 -0600

    Update during class

--- connect/harvest-1.sh
+++ connect/harvest-1.sh
@@ -11,10 +11,10 @@ bots=(
   #"1397-wallaby"  # J & L
   #"1399-wallaby"  # E & V          # USB
   #"1407-wallaby"  # M & B          # USB
-  #"1408-wallaby"  # M & M          # USB
-  "2486-wallaby"  # V & C
-  "2488-wallaby"  # M & A
-  "2494-wallaby"  # D & L
+  "1408-wallaby"  # M & M          # USB
+  #"2486-wallaby"  # V & C
+  #"2488-wallaby"  # M & A
+  #"2494-wallaby"  # D & L
 )
 
 #url="192.168.124.1"
@@ -28,14 +28,14 @@ do
   echo "Attempting to connect to $i."
   nmcli con up $i
   #ping -c 2 "$url"
-  ping -c 2 192.168.125.1
+  ping -c 2 192.168.124.1
 
   # TODO: enclose this in if-block, and skip if the connection failed.
   #   Currently, after a failed connecion, nothing is downloaded, and it moves on to the next Wallaby.
   #   (Possibly is downloads again from the previous Wallaby.)
   echo "$url"
   echo "Attempting to download files from $i over $url."
-  scp -r root@192.168.125.1:'~/Documents/KISS/Default\ User/' ~/Documents/kipr/cms-norman-jbc-2016/$i/
+  scp -r root@192.168.124.1:'~/Documents/KISS/Default\ User/' ~/Documents/kipr/cms-norman-jbc-2016/$i/
   #scp -r root@$url:'~/Documents/KISS/Default\ User/' ~/Documents/kipr/cms-norman-jbc-2016/$i/
 
   # Uncomment to simulate downloading files.
