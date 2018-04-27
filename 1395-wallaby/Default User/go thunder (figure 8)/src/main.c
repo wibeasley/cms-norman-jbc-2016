348513ef86410ee638a40b9cd9b4dbfb91a41ef7
wibeasley <wibeasley@hotmail.com>
2018-04-27 00:11:52 -0500

    weekly update

--- 1395-wallaby/Default User/go thunder (figure 8)/src/main.c
+++ 1395-wallaby/Default User/go thunder (figure 8)/src/main.c
@@ -10,13 +10,16 @@ int main(){
   	move(50,50,4000); //go forward
     move(-30,30,1500); //pivot left
     move(50,50,5000);  //go forwerd
-    move(30,-30,2250); //pivet right
+    move(30,-30,1500); //pivet right
     move(50,50,4000); //go forwerd
     move (30,-30,1000); //pivit right
     move (50,50,3000); // go forwerd
     move (30,-30,1500);//pivit left
-    
-    
+    move(50,50,3000);//go forwed 
+    move(30,-30,1000);//pivit right
+    move(50,50,3000);// go forwrd
+    move(-30,30,1500);//pivit left
+    move(50,50,5500);//go forwrd
     
     return 0;
 }
--- 1397-wallaby/Default User/rescue the can with functions/src/main.c
+++ 1397-wallaby/Default User/rescue the can with functions/src/main.c
@@ -1,5 +1,5 @@
 #include <kipr/botball.h>
-
+//AR top angle towrd can 2
 int move(int speed_right, int speed_left, int duration){
     
     motor(0,speed_right);
@@ -8,12 +8,12 @@ int move(int speed_right, int speed_left, int duration){
     return 0;
 }
 
-int put_can_down(){
+int put_can_down(int first, int second, int last){
     
     enable_servos();
-    set_servo_position(1,813);
-    set_servo_position(2,913);
-    set_servo_position(2,1290);
+    set_servo_position(1,first);//813
+    set_servo_position(2,second);//813
+    set_servo_position(2,last);//1290
     
     return 0;
 }
@@ -21,12 +21,28 @@ int main(){
 
     printf("Hello World\n");
     enable_servos();
-    set_servo_position(1,1069);
-    set_servo_position(2,1290);
+    set_servo_position(1,1150);
+    set_servo_position(2,1330);
     move(50, 50, 4000);
-    set_servo_position(1,813);
-    move(0, 50, 2500);
+    move(50, 50, 4000);
     
+    set_servo_position(2,800);
+    msleep(100);
+    ao();
+    msleep(100);
+    set_servo_position(1,713);
+    msleep(100);
+    ao();
+    msleep(100);
+    move(0, 50, 3500);
+    move(50,50,1600);
+    move(50,50,1600);
+    move(0, 50, 1600);
+    move(50,50,1600);
+    move(50,50,1600);
+    printf("put can down\n");
+    put_can_down(813, 813, 1290);
+        
     
         
     return 0;
