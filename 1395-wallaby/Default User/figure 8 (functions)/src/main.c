1deea548f518398bbeb28ccd27166de5b65073a8
Will Beasley <wibeasley@hotmail.com>
2018-05-23 14:34:33 -0500

    end of the year backups

---
+++ 1395-wallaby/Default User/figure 8 (functions)/src/main.c
@@ -0,0 +1,13 @@
+#include <kipr/botball.h>
+
+int main()
+{
+  int move (int, speed_left int, speed_right int, duration){  
+    
+    
+    printf("");
+    motor(0,move);
+    motor(3,move);
+    msleep (duratino);                                   }
+    return 0;
+}
